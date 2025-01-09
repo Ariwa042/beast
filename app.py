3f4bfe96b3ee3427c1904bf5a1aa7d8701cf9aec
Ariwa042 <116925898+Ariwa042@users.noreply.github.com>
2025-01-09 04:46:10 +0100

    A

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def submit():
         return jsonify({"error": "Invalid request method."}), 405
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
